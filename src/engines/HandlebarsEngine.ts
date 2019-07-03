26a76a9f40958403d317b3d631b742d5a96caddd
Chris Tate <chris@austincodeshop.com>
2019-07-03 11:58:17 -0500

    updates zapp core

--- src/engines/HandlebarsEngine.ts
+++ src/engines/HandlebarsEngine.ts
@@ -1,6 +1,41 @@
-import { Engine } from '@zappjs/core';
-import { compile } from 'handlebars';
+import { IEngine } from '@zappjs/core';
+import * as changeCase from 'change-case';
+import { compile, registerHelper, HelperOptions } from 'handlebars';
+import { plural, singular } from 'pluralize';
 
-export const HandlebarsEngine: Engine = (spec, template) => {
+registerHelper(
+  'case',
+  (
+    str: string,
+    casing: 'camel'
+      | 'constant'
+      | 'dot'
+      | 'header'
+      | 'lower'
+      | 'lcFirst'
+      | 'no'
+      | 'param'
+      | 'pascal'
+      | 'path'
+      | 'sentence'
+      | 'snake'
+      | 'swap'
+      | 'title'
+      | 'upper'
+      | 'ucFirst',
+    opts: HelperOptions
+  ) => {
+    if (!changeCase[casing]) {
+      throw new Error(`Unknown casing: ${casing}`);
+    }
+    return changeCase[casing](str);
+  }
+);
+
+registerHelper('plural', (str: string, opts: HelperOptions) => plural(str));
+
+registerHelper('singular', (str: string, opts: HelperOptions) => singular(str));
+
+export const HandlebarsEngine: IEngine = (spec, template) => {
   return compile(template)(spec);
-}
+};
